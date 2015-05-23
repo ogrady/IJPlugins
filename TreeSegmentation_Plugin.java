e0c1edac7ace025f45c6021c6b80a51776209086
Daniel OGrady <danielogrady@gmx.de>
2015-05-23 19:34:55 +0200

    fixed segmentation for images with odd dimensions added merge - works okay, but also merges sections that are not adjacent

--- TreeSegmentation_Plugin.java
+++ TreeSegmentation_Plugin.java
@@ -1,6 +1,13 @@
 import ij.ImagePlus;
 import ij.plugin.filter.PlugInFilter;
 import ij.process.ImageProcessor;
+
+import java.awt.Color;
+import java.util.Collection;
+import java.util.Random;
+
+import util.pixel.GreyValue;
+import util.pixel.Pixel;
 import util.tree.Segment;
 
 /**
@@ -14,14 +21,43 @@ public class TreeSegmentation_Plugin implements PlugInFilter {
 
 	@Override
 	public void run(final ImageProcessor ip) {
+		// root segment, covering the whole image, splitting itself within the
+		// constructor
 		final Segment s = new Segment(ip);
-		s.draw();
+
+		final Collection<Segment> leafs = Segment.mergeTree(s);
+		for (final Segment seg : leafs) {
+			drawSegment(seg, ip);
+		}
 		imp.updateAndDraw();
+
 	}
 
 	@Override
 	public int setup(final String arg, final ImagePlus imp) {
 		this.imp = imp;
 		return DOES_8G;
+	}
+
+	/**
+	 * Colors every pixel of a segment. The colour for one segment is randomly
+	 * generated and might therefore collide with the colour from other
+	 * segments.
+	 *
+	 * @param s
+	 *            segment to draw
+	 * @param image
+	 *            image to draw the segment on
+	 */
+	public static void drawSegment(final Segment s, final ImageProcessor image) {
+		final Random rgn = new Random();
+		final int r = rgn.nextInt(255);
+		final int g = rgn.nextInt(255);
+		final int b = rgn.nextInt(255);
+		final Color c = new Color(r, g, b);
+		image.setColor(c);
+		for (final Pixel<GreyValue> p : s.getPixels()) {
+			image.drawDot(p.getX(), p.getY());
+		}
 	}
 }
--- util/tree/Segment.java
+++ util/tree/Segment.java
@@ -5,41 +5,126 @@ import ij.process.ImageProcessor;
 import java.awt.Color;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Random;
 
 import util.pixel.GreyValue;
 import util.pixel.Pixel;
 
+/**
+ * Sections of one image, partitioning themselves into smaller sections, if
+ * needed. So this is basically a quadtree of pixels within an image.
+ * 
+ * @author Antonio Rajic, Hien Nguyen, Daniel O'Grady
+ *
+ */
 public class Segment {
 	public static final int SPLIT_THRESHOLD = 60;
+	public static final int MERGE_THRESHOLD = 60;
 
 	private final ImageProcessor image;
 	private final Collection<Segment> subsegments;
 	private final Collection<Pixel<GreyValue>> pixels;
 	// pixels with highest/ lowest grey-value
 	private Pixel<GreyValue> minValue,maxValue;
-	private final int x, y, width, height;
+	private int width, height, x, y;
 
 	/**
-	 * Adds one pixel to the segment and updates the min- and maxValue, if
-	 * needed
+	 * <p>
+	 * Merges the leafs of a segment-quadtree (= smallest undivisible segments
+	 * of the original image) into bigger segments. As this process destroys the
+	 * tree-structure, as we might merge nodes from different layers and
+	 * different subtrees, the segments are returned in a collection.
+	 * </p>
+	 * <p>
+	 * Important: as there seems to be no easy way to determine whether two
+	 * nodes of the quadtree are physically neighbouring (especially when they
+	 * take on weird shapes from early merges, like L- or T-shapes, the
+	 * algorithm just merges all sections that are similar - probably even
+	 * sections that are not sitting next to each other. *
+	 * </p>
 	 *
-	 * @param pixel
-	 *            pixel to add
+	 * @param root
+	 *            uppermost root from which to start
+	 * @return collection of merged segments
 	 */
-	public void addPixel(final Pixel<GreyValue> pixel) {
-		final boolean success = this.pixels.add(pixel);
-		if(success) {
-			if(minValue == null || pixel.getData().greyValue < minValue.getData().greyValue) {
-				minValue = pixel;
-			}
-			if(maxValue == null || pixel.getData().greyValue > maxValue.getData().greyValue) {
-				maxValue = pixel;
+	public static Collection<Segment> mergeTree(final Segment root) {
+		// we're basically comparing all leafs pairwise, merging similar
+		// sections into one, removing the other section. This is repeated until
+		// we couldn't find more regions to merge.
+		final Collection<Segment> leafs = root.getLeafs();
+		boolean merged = true;
+		while (merged) {
+			merged = false;
+			final Segment[] leafsAr = new Segment[leafs.size()];
+			leafs.toArray(leafsAr);
+			int i = 0;
+			while (!merged && i < leafsAr.length) {
+				int j = i + 1;
+				while (!merged && j < leafsAr.length) {
+					if (i != j
+							&& leafsAr[i].distance(leafsAr[j]) < MERGE_THRESHOLD) {
+						leafsAr[i].mergeWith(leafsAr[j]);
+						leafs.remove(leafsAr[j]);
+						merged = true;
+					}
+					j++;
+				}
+				i++;
 			}
 		}
+		return leafs;
 	}
 
 	/**
-	 * Constructor. Convienience-method for the uppermost segment of a picture.
+	 * @return max grey-value within this segment (0 if empty)
+	 */
+	public int getMaxGreyValue() {
+		return maxValue != null ? maxValue.getData().greyValue : 0;
+	}
+
+	/**
+	 * @return min grey-value within this segment (0 if empty)
+	 */
+	public int getMinGreyValue() {
+		return minValue != null ? minValue.getData().greyValue : 0;
+	}
+
+	/**
+	 * @return whether this segment is a leaf (= not split)
+	 */
+	public boolean isLeaf() {
+		return subsegments.isEmpty();
+	}
+
+	/**
+	 * @return a collection of all the pixels in this segment
+	 */
+	public Collection<Pixel<GreyValue>> getPixels() {
+		return pixels;
+	}
+
+	/**
+	 * @return a list of all leafs below this segment. This includes all
+	 *         descendants, not just direct children
+	 */
+	public Collection<Segment> getLeafs() {
+		final Collection<Segment> leafs = new ArrayList<Segment>();
+		if (isLeaf()) {
+			// division might have yielded empty segments -> ignore them
+			if (!pixels.isEmpty()) {
+				leafs.add(this);
+			}
+		} else {
+			for (final Segment sub : subsegments) {
+				leafs.addAll(sub.getLeafs());
+			}
+		}
+		return leafs;
+	}
+
+	/**
+	 * Constructor. Convenience-method for the uppermost segment of a picture.
 	 * See {@link #Segment(ImageProcessor, int, int, int, int)}.
 	 */
 	public Segment(final ImageProcessor ip) {
@@ -64,7 +149,7 @@ public class Segment {
 	 */
 	public Segment(final ImageProcessor ip, final int x, final int y,
 			final int w, final int h) {
-		subsegments = new ArrayList<Segment>(4);
+		subsegments = new HashSet<Segment>(4);
 		pixels = new ArrayList<Pixel<GreyValue>>(w * h);
 		image = ip;
 		this.x = x;
@@ -80,48 +165,126 @@ public class Segment {
 			}
 		}
 		// segments consisting of just one pixel don't need to be splitted
-		if (pixels.size() > 0
-				&& maxValue.getData().greyValue - minValue.getData().greyValue > SPLIT_THRESHOLD) {
+		if (pixels.size() > 1 && interval() > SPLIT_THRESHOLD) {
 			divide();
 		}
 	}
 
 	/**
-	 * @return a collection of all the pixels in this segment
+	 * Adds one pixel to the segment and updates the min- and maxValue and
+	 * coordinates, if needed
+	 *
+	 * @param pixel
+	 *            pixel to add
 	 */
-	public Collection<Pixel<GreyValue>> getPixels() {
-		return this.pixels;
+	public void addPixel(final Pixel<GreyValue> pixel) {
+		if (pixels.add(pixel)) {
+			if (minValue == null || pixel.getData().greyValue < minValue.getData().greyValue) {
+				minValue = pixel;
+			}
+			if (maxValue == null || pixel.getData().greyValue > maxValue.getData().greyValue) {
+				maxValue = pixel;
+			}
+			if (pixel.getX() < x) {
+				x = pixel.getX();
+			}
+			if (pixel.getY() < y) {
+				y = pixel.getY();
+			}
+			if (pixel.getX() > x + width) {
+				width = pixel.getX() - x;
+			}
+			if (pixel.getY() > y + height) {
+				height = pixel.getY() - y;
+			}
+		}
 	}
 
 	/**
 	 * Divides the segment into four subsegments of equal size
 	 */
 	public void divide() {
-		final int w = width / 2;
-		final int h = height / 2;
+		final int w = (int) Math.floor(width / 2);
+		final int h = (int) Math.floor(height / 2);
 
+		// calculating the dimensions for the subsegments like
+		// this fixes segments with odd pixelcount. We might get empty segments
+		// width w/h=0, but that doesn't cause us any troubles.
 		subsegments.add(new Segment(image, x, y, w, h));
-		subsegments.add(new Segment(image, x + w, y, w, h));
-		subsegments.add(new Segment(image, x, y + h, w, h));
-		subsegments.add(new Segment(image, x + w, y + h, w, h));
+		subsegments.add(new Segment(image, x + w, y, width - w, h));
+		subsegments.add(new Segment(image, x, y + h, w, height - h));
+		subsegments.add(new Segment(image, x + w, y + h, width - w, height - h));
 	}
 
 	/**
 	 * @param other
 	 *            the other segment to unite this segment with
 	 */
-	public void unite(final Segment other) {
-		this.pixels.addAll(other.getPixels());
+	public void mergeWith(final Segment other) {
+		if (!other.equals(this)) {
+			for (final Pixel<GreyValue> p : other.getPixels()) {
+				addPixel(p);
+			}
+		}
+	}
+
+	/**
+	 * @return size of the interval of greyvalues within the segment (0 if
+	 *         empty)
+	 */
+	public int interval() {
+		return maxValue != null && minValue != null ? maxValue.getData().greyValue
+				- minValue.getData().greyValue
+				: 0;
+	}
+
+	/**
+	 * @param other
+	 *            other segment to compare this segment to
+	 * @return the distance between two segments in regards to their
+	 *         grey-value-interval (not physical distance within the picture!)
+	 */
+	public int distance(final Segment other) {
+		return Math.abs(getMaxGreyValue() - other.getMaxGreyValue());
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		boolean equal = false;
+		if (obj instanceof Segment) {
+			final Segment s = (Segment) obj;
+			equal = super.equals(obj) || s.x == x && s.y == y
+					&& s.width == width && s.height == height;
+		}
+		return equal;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString()
+				+ String.format(" (%d|%d) [w:%d h:%d] size=%d", x, y, width, height,
+						pixels.size());
 	}
 
 	/**
 	 * draws the segment (debugging only)
 	 */
+	@Deprecated
 	public void draw() {
-		image.setColor(Color.GREEN);
+		final Random rgn = new Random();
+		final int r = rgn.nextInt(255);
+		final int g = rgn.nextInt(255);
+		final int b = rgn.nextInt(255);
+		final Color c = new Color(r, g, b);
+		image.setColor(c);
+		if (isLeaf()) {
+			for (final Pixel<GreyValue> p : pixels) {
+				image.drawDot(p.getX(), p.getY());
+			}
+		}
 		for (final Segment s : subsegments) {
 			s.draw();
 		}
-		image.drawRect(x, y, width, height);
+
 	}
 }
