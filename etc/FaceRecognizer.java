cb67342c4e55f7e71cb06f52f82fa89dbb37d1d4
Daniel OGrady <danielogrady@gmx.de>
2015-06-30 08:54:30 +0200

    added means to store regocnized faces

--- etc/FaceRecognizer.java
+++ etc/FaceRecognizer.java
@@ -1,11 +1,13 @@
 package etc;
 
 import ij.ImagePlus;
+import ij.io.FileSaver;
 import ij.io.Opener;
 import ij.process.ImageProcessor;
 
 import java.awt.Point;
 import java.io.File;
+import java.util.LinkedList;
 
 import util.facerecognition.DTW;
 
@@ -13,20 +15,27 @@ public class FaceRecognizer {
 	public static void main(final String[] args) {
 		final String subjectpath = "C:/Users/Daniel/Desktop/faces/subjects";
 		final String templatepath = "C:/Users/Daniel/Desktop/faces/templates";
+		final String outputpath = "C:/Users/Daniel/Desktop/faces/output/";
+		final boolean output = true;
 		final File[] subjects = new File(subjectpath).listFiles();
 		final File[] templates = new File(templatepath).listFiles();
+		final LinkedList<ImagePlus> subjectImages = new LinkedList<ImagePlus>();
 
-		ImagePlus subjectImage;
 		ImageProcessor sip, tip;
 		final Opener opener = new Opener();
 		for (final File template : templates) {
 			tip = opener.openImage(template.getAbsolutePath()).getProcessor();
 			for (final File subject : subjects) {
-				subjectImage = opener.openImage(subject.getAbsolutePath());
-				sip = subjectImage.getProcessor();
+				subjectImages.add(opener.openImage(subject.getAbsolutePath()));
+				sip = subjectImages.getLast().getProcessor();
 				final Point p = DTW.findMatch(sip, tip);
 				sip.drawRect(p.x, p.y, tip.getWidth(), tip.getHeight());
-				subjectImage.show();
+				subjectImages.getLast().show();
+				if(output) {
+					final FileSaver saver = new FileSaver(subjectImages.getLast());
+					saver.saveAsJpeg(outputpath + subject.getName() + "__" + template.getName()
+							+ ".jpg");
+				}
 			}
 		}
 	}
